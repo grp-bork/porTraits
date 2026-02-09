545808e4cdd163c07b23a903321e33ae9338a051
Christian Schudoma <christian.schudoma@embl.de>
2026-02-09 18:27:00 +0100

    cleanup

--- bin/metatraits_comm.py
+++ bin/metatraits_comm.py
@@ -11,7 +11,6 @@ METATRAITS_URL = "https://metatraits.embl.de"
 METATRAITS_API_URL = f"{METATRAITS_URL}/api/v1"
 
 
-
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("--speci", type=str)
@@ -46,89 +45,25 @@ def main():
     # https://metatraits.embl.de/taxonomy/download?query=Bacteroides+uniformis&rank=species
     url = f"{METATRAITS_URL}/taxonomy/download"
     params = {
-        # "query": re.sub(r' +', "+", name),
         "query": taxname,
         "rank": rank,
         "taxonomy": taxonomy,
     }
     headers = {
-        # "Content-Disposition": f"attachment; filename=summary_{tax_rank}_{tax_query}.json"
-        "Content-Disposition": "inline"
+        "Content-Disposition": "inline",
     }
 
     request = requests.get(url, params=params, headers=headers)
 
     with open(output_fn, 'wb') as json_out:
         if request:
-
             print(request.url)
-            # if args.lineage:
-            #     print(request.json())
-
+            
             for chunk in request.iter_content(chunk_size=8192):
                 json_out.write(chunk)
 
 
     return None
-    if args.speci:
-        url = f"{METATRAITS_API_URL}/species_taxonomy/{args.speci}"
-        # print(url)
-        request = requests.get(url)
-        d = request.json()
-        #  {'species_name': 'Escherichia coli', 'species_tax_id': '562'})
-
-
-        # print(d)
-        url = f"{METATRAITS_API_URL}/traits/taxonomy/{d['species_tax_id']}"
-        request = requests.get(url, stream=True,)
-
-        # d = request.json()
-        # with open(args.output, 'wb') as json_out:
-        #     for chunk in request.iter_content(chunk_size=8192):
-        #         json_out.write(chunk)
-
-        # print(json.dumps(request.json(), indent=4))
-
-    elif args.lineage:
-
-        # d__Bacteria;p__Bacteroidota;c__Kapaibacteriia;o__Kapaibacteriales;f__Kapaibacteriaceae;g__Kapaibacterium;s__
-        lineage = [
-            item.split("__") for item in args.lineage.strip().split(";") if item[-2:] != "__"
-        ]
-        if not lineage:
-            request = None
-        else:
-            rank, name = lineage[-1]
-
-            # https://metatraits.embl.de/taxonomy/download?query=Bacteroides+uniformis&rank=species
-            url = f"{METATRAITS_URL}/taxonomy/download"
-            params = {
-                # "query": re.sub(r' +', "+", name),
-                "query": name,
-                "rank": {"d": "domain", "p": "phylum", "c": "class", "o": "order", "f": "family", "g": "genus", "s": "species",}.get(rank, "species"),
-                "taxonomy": "gtdb",
-            }
-            headers = {
-                # "Content-Disposition": f"attachment; filename=summary_{tax_rank}_{tax_query}.json"
-                "Content-Disposition": "inline"
-            }
-
-            request = requests.get(url, params=params, headers=headers)
-            # d = request.json()
-
-    else:
-        raise ValueError("No param.")
-
-    with open(args.output, 'wb') as json_out:
-        if request:
-
-            print(request.url)
-            if args.lineage:
-                print(request.json())
-
-            for chunk in request.iter_content(chunk_size=8192):
-                json_out.write(chunk)
-
 
 
 if __name__ == "__main__":
